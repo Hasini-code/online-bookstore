380667e9d9d8381bff6cad7751a8ab96fae7088d
ravireni4148 <169068080+ravireni4148@users.noreply.github.com>
2025-06-22 15:51:55 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,17 +1,18 @@
-const showRegister = document.getElementById("showRegister");
+// Toggle between login and register forms
 const showLogin = document.getElementById("showLogin");
+const showRegister = document.getElementById("showRegister");
 const loginForm = document.getElementById("loginForm");
 const registerForm = document.getElementById("registerForm");
 const formTitle = document.getElementById("form-title");
+
+showLogin.addEventListener("click", () => {
+  loginForm.style.display = "flex";
+  registerForm.style.display = "none";
+  formTitle.textContent = "Login";
+});
 
 showRegister.addEventListener("click", () => {
   loginForm.style.display = "none";
   registerForm.style.display = "flex";
   formTitle.textContent = "Register";
-});
-
-showLogin.addEventListener("click", () => {
-  registerForm.style.display = "none";
-  loginForm.style.display = "flex";
-  formTitle.textContent = "Login";
 });
